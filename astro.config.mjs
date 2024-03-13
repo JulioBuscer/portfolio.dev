2809a9e1a183f04afb8023830a8adea25a2658e9
JulioBuscer <juliobuscer@gmail.com>
2024-03-13 08:34:16 -0600

    se agrega y configura i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,23 @@
 import { defineConfig } from 'astro/config';
+import astroI18next from 'astro-i18next';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false
+    },
+    fallback: {
+      'en': 'es'
+    }
+  }
+
+},
+);
+
---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+    es: 'Español',
+    en: 'English'
+}
+
+export const defaultLang = 'es'
+
+export const ui = {
+    es: {
+        'nav.home': 'Inicio',
+        'nav.experience': 'Experiencia',
+        'nav.projects': 'Proyectos',
+        'nav.about': 'Sobre mi',
+        'nav.contact': 'Contacto',
+        'nav.cv': 'CV Minimalista',
+    },
+    en: {
+        'nav.home': 'Home',
+        'nav.experience': 'Experience',
+        'nav.projects': 'Projects',
+        'nav.about': 'About',
+        'nav.contact': 'Contact',
+        'nav.cv': 'CV Minimalist',
+    }
+} as const
