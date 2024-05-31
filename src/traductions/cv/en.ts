50d0b66463ee73df6ddcd62adba5b09cc79c3489
Julio Bustamante <69023054+JulioBuscer@users.noreply.github.com>
2024-05-31 15:11:00 -0600

    actualizacion de datos

--- src/traductions/cv/en.ts
+++ src/traductions/cv/en.ts
@@ -1,202 +1,162 @@
-import type { CvData } from '../../interfaces/CvData.ts';
+import type { CvData } from "../../interfaces/CvData.ts";
 export const cv: CvData = {
-    "basics": {
-        "name": "Julio Bustamante",
-        "label": "Programmer & Developer Web",
-        "image": "https://media.licdn.com/dms/image/D5603AQEo0AFz5bIYZA/profile-displayphoto-shrink_800_800/0/1705929035623?e=1718236800&v=beta&t=D4pFn6AawcQ5207uK-FxBR5NgEJ6PgklASND48csuBc",
-        "email": "juliobuscer@gmail.com",
-        "phone": "+52 (477) 704-6131",
-        "url": "https://johndoe.com",
-        "summary": "years of experience as a developer. Specialized in developing web solutions",
-        "location": {
-            "address": "N/A",
-            "postalCode": "N/A",
-            "city": "León",
-            "country": "México",
-            "countryCode": "MX",
-            "region": "Guanajuato"
-        },
-        "profiles": [
-            {
-                "network": "LinkedIn",
-                "username": "Julio Bustamante",
-                "url": "https://www.linkedin.com/in/juliobuscer/"
-            },
-            {
-                "network": "Github",
-                "username": "juliobuscer",
-                "url": "https://Github.com/juliobuscer"
-            }
-        ]
+  basics: {
+    name: "Julio Bustamante",
+    label: "Programmer & Developer Web",
+    image:
+      "https://media.licdn.com/dms/image/D5603AQEo0AFz5bIYZA/profile-displayphoto-shrink_800_800/0/1705929035623?e=1718236800&v=beta&t=D4pFn6AawcQ5207uK-FxBR5NgEJ6PgklASND48csuBc",
+    email: "juliobuscer@gmail.com",
+    phone: "+52 (477) 704-6131",
+    url: "https://johndoe.com",
+    summary:
+      "years of experience as a developer. Specialized in developing web solutions. I stand out for my ability to quickly assimilate new knowledge and apply it efficiently.",
+    location: {
+      address: "N/A",
+      postalCode: "N/A",
+      city: "León",
+      country: "México",
+      countryCode: "MX",
+      region: "Guanajuato",
     },
-    "work": [
-        {
-            "name": "CONGLOMERADO DE TECNOLOGÍAS AVANZADAS S.A. de C.V.",
-            "position": "FullStack developer",
-            "url": "https://company.com",
-            "startDate": "2023-06-01",
-            "endDate": null,
-            "summary": "Development of web solutions using Frontend and Backend applications.",
-            "highlights": [
-                "Se crea la subsidiaría de Novatec enfocada al desarrollo"
-            ]
-        },
-        {
-            "name": "Novatec Group",
-            "position": "FullStack developer",
-            "url": "https://company.com",
-            "startDate": "2022-01-01",
-            "endDate": "2023-06-01",
-            "summary": "Development of web solutions using Frontend and Backend applications.",
-            "highlights": []
-        },
-        {
-            "name": "Novatec Group",
-            "position": "FullStack Developer Intern",
-            "url": "https://company.com",
-            "startDate": "2021-07-01",
-            "endDate": "2022-01-01",
-            "summary": "Development of web solutions using Frontend and Backend applications.",
-            "highlights": []
-        }
+    profiles: [
+      {
+        network: "LinkedIn",
+        username: "Julio Bustamante",
+        url: "https://www.linkedin.com/in/juliobuscer/",
+      },
+      {
+        network: "Github",
+        username: "juliobuscer",
+        url: "https://Github.com/juliobuscer",
+      },
     ],
-    "volunteer": [
-        {
-            "organization": "Organization",
-            "position": "Volunteer",
-            "url": "https://organization.com/",
-            "startDate": "2012-01-01",
-            "endDate": "2013-01-01",
-            "summary": "Description…",
-            "highlights": [
-                "Awarded 'Volunteer of the Month'"
-            ]
-        }
-    ],
-    "education": [
-        {
-            "institution": "Universidad Tecnológica de León",
-            "url": "https://www.utleon.edu.mx/",
-            "area": "Software Development and Management.",
-            "studyType": "Engineer",
-            "startDate": "2018-09-01",
-            "endDate": "2022-08-01",
-            "score": "9.0",
-            "courses": [
-                "DB1101 - Basic SQL"
-            ]
-        }
-    ],
-    "awards": [
-        {
-            "title": "Award",
-            "date": "2014-11-01",
-            "awarder": "Company",
-            "summary": "There is no spoon."
-        }
-    ],
-    "certificates": [
-        {
-            "name": "Certificate",
-            "date": "2021-11-07",
-            "issuer": "Company",
-            "url": "https://certificate.com"
-        }
-    ],
-    "publications": [
-        {
-            "name": "Publication",
-            "publisher": "Company",
-            "releaseDate": "2014-10-01",
-            "url": "https://publication.com",
-            "summary": "Description…"
-        }
-    ],
-    "skills": [
-        {
-            "name": "Frameworks",
-            "level": "Advanced",
-            "keywords": [
-                "React",
-                "Astro",
-                "JSF"
-            ]
-        },
-        {
-            "name": "Frontend",
-            "level": "Advanced",
-            "keywords": [
-                "HTML",
-                "CSS",
-                "JavaScript"
-            ]
-        },
-        {
-            "name": "Backend",
-            "level": "Advanced",
-            "keywords": [
-                "Java",
-                "Node.js"
-            ]
-        },
-        {
-            "name": "Data Bases",
-            "level": "Advanced",
-            "keywords": [
-                "SQL Server",
-                "MySQL",
-                "MongoDB"
-            ]
-        },
-        {
-            "name": "DevOps",
-            "level": "Advanced",
-            "keywords": [
-                "GitHub"
-            ]
-        }
-    ],
-    "languages": [
-        {
-            "language": "Español",
-            "fluency": "Nativo"
-        },
-        {
-            "language": "English",
-            "fluency": "B2"
-        }
-    ],
-    "interests": [
-        {
-            "name": "Wildlife",
-            "keywords": [
-                "Ferrets",
-                "Unicorns"
-            ]
-        }
-    ],
-    "references": [
-        {
-            "name": "Jane Doe",
-            "reference": "Reference…"
-        }
-    ],
-    "projects": [
-        {
-            "name": "Offer Form",
-            "startDate": "2022-01-01",
-            "endDate": null,
-            "description": "Product developed for internal use of the company Novatec, provides a solution to the estimation and monitoring of projects offered to its customers. It has reports that support and speed up the decision making process.",
-            "highlights": [
-                "JSF",
-                "XHTML",
-                "CSS",
-                "Java",
-                "JavaScript",
-                "SQLServer"
-            ],
-            "url": "",
-            "image": "/projects/Project-OfferForm.webp"
-        }
-    ]
-}
+  },
+  work: [
+    {
+      name: "Novatec Group",
+      position: "FullStack developer",
+      url: "https://company.com",
+      startDate: "2021-07-01",
+      endDate: "2024-05-24",
+      summary: "",
+      highlights: [
+        "Worked independently or in a team",
+        "Collaboration with the CEO and the Marketing department to understand the requirements and specifications of business in the management of customer offers, risks and analysis",
+        "Collaboration with the production area to understand the requirements and business specificities around customer offerings, analysis and risk management",
+        "Troubleshooting using the Web stack required by the company (Java, Netbeans IDE, SQL Server, Payara Server)",
+        "Implementation and update of application modules under the direction of the Porject Manager",
+        "Effective coding of software changes and enhancements based on concrete specifications",
+        "Development and presentation of findings and solutions to senior executives and stakeholders",
+      ],
+    },
+  ],
+  volunteer: [
+    {
+      organization: "Organization",
+      position: "Volunteer",
+      url: "https://organization.com/",
+      startDate: "2012-01-01",
+      endDate: "2013-01-01",
+      summary: "Description…",
+      highlights: ["Awarded 'Volunteer of the Month'"],
+    },
+  ],
+  education: [
+    {
+      institution: "Universidad Tecnológica de León",
+      url: "https://www.utleon.edu.mx/",
+      area: "Software Development and Management.",
+      studyType: "Engineer",
+      startDate: "2018-09-01",
+      endDate: "2022-08-01",
+      score: "9.0",
+      courses: ["DB1101 - Basic SQL"],
+    },
+  ],
+  awards: [
+    {
+      title: "Award",
+      date: "2014-11-01",
+      awarder: "Company",
+      summary: "There is no spoon.",
+    },
+  ],
+  certificates: [
+    {
+      name: "Certificate",
+      date: "2021-11-07",
+      issuer: "Company",
+      url: "https://certificate.com",
+    },
+  ],
+  publications: [
+    {
+      name: "Publication",
+      publisher: "Company",
+      releaseDate: "2014-10-01",
+      url: "https://publication.com",
+      summary: "Description…",
+    },
+  ],
+  skills: [
+    {
+      name: "Frameworks",
+      level: "Advanced",
+      keywords: ["React", "Astro", "JSF"],
+    },
+    {
+      name: "Frontend",
+      level: "Advanced",
+      keywords: ["HTML", "CSS", "JavaScript"],
+    },
+    {
+      name: "Backend",
+      level: "Advanced",
+      keywords: ["Java", "Node.js"],
+    },
+    {
+      name: "Data Bases",
+      level: "Advanced",
+      keywords: ["SQL Server", "MySQL", "MongoDB"],
+    },
+    {
+      name: "DevOps",
+      level: "Advanced",
+      keywords: ["GitHub"],
+    },
+  ],
+  languages: [
+    {
+      language: "Español",
+      fluency: "Nativo",
+    },
+    {
+      language: "English",
+      fluency: "B2",
+    },
+  ],
+  interests: [
+    {
+      name: "Wildlife",
+      keywords: ["Ferrets", "Unicorns"],
+    },
+  ],
+  references: [
+    {
+      name: "Jane Doe",
+      reference: "Reference…",
+    },
+  ],
+  projects: [
+    {
+      name: "Offer Form",
+      startDate: "2022-01-01",
+      endDate: null,
+      description:
+        "Product developed for internal use of the company Novatec, provides a solution to the estimation and monitoring of projects offered to its customers. It has reports that support and speed up the decision making process.",
+      highlights: ["JSF", "XHTML", "CSS", "Java", "JavaScript", "SQLServer"],
+      url: "",
+      image: "/projects/Project-OfferForm.webp",
+    },
+  ],
+};
--- src/traductions/cv/es.ts
+++ src/traductions/cv/es.ts
@@ -1,215 +1,161 @@
-import type { CvData } from '../../interfaces/CvData.ts';
-export const cv: CvData =
-{
-    "basics": {
-        "name": "Julio Bustamante",
-        "label": "Programador y Desarrollador Web",
-        "image": "https://media.licdn.com/dms/image/D5603AQEo0AFz5bIYZA/profile-displayphoto-shrink_800_800/0/1705929035623?e=1718236800&v=beta&t=D4pFn6AawcQ5207uK-FxBR5NgEJ6PgklASND48csuBc",
-        "email": "juliobuscer@gmail.com",
-        "phone": "+52 (477) 704-6131",
-        "url": "https://johndoe.com",
-        "summary": "años de experiencia como desarrollador. Especializado en desarrollar soluciones web",
-        "location": {
-            "address": "N/A",
-            "postalCode": "N/A",
-            "city": "León",
-            "country": "México",
-            "countryCode": "MX",
-            "region": "Guanajuato"
-        },
-        "profiles": [
-            {
-                "network": "LinkedIn",
-                "username": "Julio Bustamante",
-                "url": "https://www.linkedin.com/in/juliobuscer/"
-            },
-            {
-                "network": "Github",
-                "username": "juliobuscer",
-                "url": "https://Github.com/juliobuscer"
-            }
-        ]
+import type { CvData } from "../../interfaces/CvData.ts";
+export const cv: CvData = {
+  basics: {
+    name: "Julio Bustamante",
+    label: "Programador y Desarrollador Web",
+    image:
+      "https://media.licdn.com/dms/image/D5603AQEo0AFz5bIYZA/profile-displayphoto-shrink_800_800/0/1705929035623?e=1718236800&v=beta&t=D4pFn6AawcQ5207uK-FxBR5NgEJ6PgklASND48csuBc",
+    email: "juliobuscer@gmail.com",
+    phone: "+52 (477) 704-6131",
+    url: "https://johndoe.com",
+    summary:
+      "años de experiencia como desarrollador. Especializado en desarrollar soluciones web. Destaco por mi habilidad para asimilar rápidamente nuevos conocimientos y aplicarlos eficientemente.",
+    location: {
+      address: "N/A",
+      postalCode: "N/A",
+      city: "León",
+      country: "México",
+      countryCode: "MX",
+      region: "Guanajuato",
     },
-    "work": [
-        {
-            "name": "CONGLOMERADO DE TECNOLOGÍAS AVANZADAS S.A. de C.V.",
-            "position": "Desarrollador FullStack",
-            "url": "https://company.com",
-            "startDate": "2023-06-01",
-            "endDate": null,
-            "summary": "Desarrollo soluciones web aplicando Frontend y Backend",
-            "highlights": [
-                "Se crea la subsidiaría de Novatec enfocada al desarrollo"
-            ]
-        },
-        {
-            "name": "Novatec Group",
-            "position": "Desarrollador FullStack",
-            "url": "https://company.com",
-            "startDate": "2022-01-01",
-            "endDate": "2023-06-01",
-            "summary": "Desarrollo soluciones web aplicando Frontend y Backend",
-            "highlights": []
-        },
-        {
-            "name": "Novatec Group",
-            "position": "Practicante Desarrollador FullStack",
-            "url": "https://company.com",
-            "startDate": "2021-07-01",
-            "endDate": "2022-01-01",
-            "summary": "Desarrollo soluciones web aplicando Frontend y Backend",
-            "highlights": []
-        }
+    profiles: [
+      {
+        network: "LinkedIn",
+        username: "Julio Bustamante",
+        url: "https://www.linkedin.com/in/juliobuscer/",
+      },
+      {
+        network: "Github",
+        username: "juliobuscer",
+        url: "https://Github.com/juliobuscer",
+      },
     ],
-    "volunteer": [
-        {
-            "organization": "Organization",
-            "position": "Volunteer",
-            "url": "https://organization.com/",
-            "startDate": "2012-01-01",
-            "endDate": "2013-01-01",
-            "summary": "Description…",
-            "highlights": [
-                "Awarded 'Volunteer of the Month'"
-            ]
-        }
-    ],
-    "education": [
-        {
-            "institution": "Universidad Tecnológica de León",
-            "url": "https://www.utleon.edu.mx/",
-            "area": "Desarrollo y Gestión de Software.",
-            "studyType": "Ingeniería",
-            "startDate": "2020-09-01",
-            "endDate": "2022-08-01",
-            "score": "9.0",
-            "courses": [
-                "DB1101 - Basic SQL"
-            ]
-        },
-        {
-            "institution": "Universidad Tecnológica de León",
-            "url": "https://www.utleon.edu.mx/",
-            "area": "Desarrollo de Software Multiplataforma",
-            "studyType": "Técnico Superior Universitario",
-            "startDate": "2018-09-01",
-            "endDate": "2020-08-30",
-            "score": "4.0",
-            "courses": [
-                "DB1101 - Basic SQL"
-            ]
-        }
-    ],
-    "awards": [
-        {
-            "title": "Award",
-            "date": "2014-11-01",
-            "awarder": "Company",
-            "summary": "There is no spoon."
-        }
-    ],
-    "certificates": [
-        {
-            "name": "Certificate",
-            "date": "2021-11-07",
-            "issuer": "Company",
-            "url": "https://certificate.com"
-        }
-    ],
-    "publications": [
-        {
-            "name": "Publication",
-            "publisher": "Company",
-            "releaseDate": "2014-10-01",
-            "url": "https://publication.com",
-            "summary": "Description…"
-        }
-    ],
-    "skills": [
-        {
-            "name": "Frameworks",
-            "level": "Advanced",
-            "keywords": [
-                "React",
-                "Astro",
-                "JSF"
-            ]
-        },
-        {
-            "name": "Frontend",
-            "level": "Advanced",
-            "keywords": [
-                "HTML",
-                "CSS",
-                "JavaScript"
-            ]
-        },
-        {
-            "name": "Backend",
-            "level": "Advanced",
-            "keywords": [
-                "Java",
-                "Node.js"
-            ]
-        },
-        {
-            "name": "Bases de Datos",
-            "level": "Advanced",
-            "keywords": [
-                "SQL Server",
-                "MySQL",
-                "MongoDB"
-            ]
-        },
-        {
-            "name": "DevOps",
-            "level": "Advanced",
-            "keywords": [
-                "GitHub"
-            ]
-        }
-    ],
-    "languages": [
-        {
-            "language": "Español",
-            "fluency": "Nativo"
-        },
-        {
-            "language": "English",
-            "fluency": "B2"
-        }
-    ],
-    "interests": [
-        {
-            "name": "Wildlife",
-            "keywords": [
-                "Ferrets",
-                "Unicorns"
-            ]
-        }
-    ],
-    "references": [
-        {
-            "name": "Jane Doe",
-            "reference": "Reference…"
-        }
-    ],
-    "projects": [
-        {
-            "name": "Offer Form",
-            "startDate": "2022-01-01",
-            "endDate": null,
-            "description": "Producto desarrollado para uso interno de la empresa Novatec, da solución a la estimación y seguimiento de proyectos ofertados a sus clientes. Cuenta con reportes que apoyan y agilizan la toma de decisiones",
-            "highlights": [
-                "JSF",
-                "XHTML",
-                "CSS",
-                "Java",
-                "JavaScript",
-                "SQL Server"
-            ],
-            "url": "",
-            "image": "/projects/Project-OfferForm.webp"
-        }
-    ]
-}
+  },
+  work: [
+    {
+      name: "Novatec Group",
+      position: "Desarrollador Java FullStack",
+      url: "https://company.com",
+      startDate: "2021-07-01",
+      endDate: "2024-05-24",
+      summary: "",
+      highlights: [
+        "Trabaje tanto a nivel independiente como en equipo",
+        "Colaboracion con Ejecutivos Directivos y el area de producción para conocer los requicitos y las especiicaciónes empresariales en torno a la gestion de las ofertas a cliente, analisis y riesgos",
+        "Resolución de problemas utilizando el stack Web requerido por la empresa (Java, IDE Netbeans, SQL Server, Payara Server)",
+        "Implementación y actualización de modulos de aplicación bajo la direccion del Porject Manager",
+        "Codificción eficaz de cambios y mejoras del software en base a especificaciónes concretas",
+        "Desarrollo y presentacion de concluciones y soluciones a altos ejecutivos y partes interesadas",
+      ],
+    },
+  ],
+  volunteer: [
+    {
+      organization: "Organization",
+      position: "Volunteer",
+      url: "https://organization.com/",
+      startDate: "2012-01-01",
+      endDate: "2013-01-01",
+      summary: "Description…",
+      highlights: ["Awarded 'Volunteer of the Month'"],
+    },
+  ],
+  education: [
+    {
+      institution: "Universidad Tecnológica de León",
+      url: "https://www.utleon.edu.mx/",
+      area: "Desarrollo y Gestión de Software.",
+      studyType: "Ingeniería",
+      startDate: "2018-09-01",
+      endDate: "2022-08-01",
+      score: "9.0",
+      courses: ["DB1101 - Basic SQL"],
+    },
+  ],
+  awards: [
+    {
+      title: "Award",
+      date: "2014-11-01",
+      awarder: "Company",
+      summary: "There is no spoon.",
+    },
+  ],
+  certificates: [
+    {
+      name: "Certificate",
+      date: "2021-11-07",
+      issuer: "Company",
+      url: "https://certificate.com",
+    },
+  ],
+  publications: [
+    {
+      name: "Publication",
+      publisher: "Company",
+      releaseDate: "2014-10-01",
+      url: "https://publication.com",
+      summary: "Description…",
+    },
+  ],
+  skills: [
+    {
+      name: "Frameworks",
+      level: "Advanced",
+      keywords: ["React", "Astro", "JSF"],
+    },
+    {
+      name: "Frontend",
+      level: "Advanced",
+      keywords: ["HTML", "CSS", "JavaScript"],
+    },
+    {
+      name: "Backend",
+      level: "Advanced",
+      keywords: ["Java", "Node.js"],
+    },
+    {
+      name: "Bases de Datos",
+      level: "Advanced",
+      keywords: ["SQL Server", "MySQL", "MongoDB"],
+    },
+    {
+      name: "DevOps",
+      level: "Advanced",
+      keywords: ["GitHub"],
+    },
+  ],
+  languages: [
+    {
+      language: "Español",
+      fluency: "Nativo",
+    },
+    {
+      language: "English",
+      fluency: "B2",
+    },
+  ],
+  interests: [
+    {
+      name: "Wildlife",
+      keywords: ["Ferrets", "Unicorns"],
+    },
+  ],
+  references: [
+    {
+      name: "Jane Doe",
+      reference: "Reference…",
+    },
+  ],
+  projects: [
+    {
+      name: "Offer Form",
+      startDate: "2022-01-01",
+      endDate: null,
+      description:
+        "Producto desarrollado para uso interno de la empresa Novatec, da solución a la estimación y seguimiento de proyectos ofertados a sus clientes. Cuenta con reportes que apoyan y agilizan la toma de decisiones",
+      highlights: ["JSF", "XHTML", "CSS", "Java", "JavaScript", "SQL Server"],
+      url: "",
+      image: "/projects/Project-OfferForm.webp",
+    },
+  ],
+};
